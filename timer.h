38a53a7086524e2a48cb3f3c95633ce068a42c8c
lanhtao <lanhtao@users.noreply.github.com>
2015-07-17 10:01:14 +0800

    Update timer.h

--- timer.h
+++ timer.h
@@ -2,28 +2,42 @@
 #define SRC_TIMER_H_
 #include "common.h"
 
-typedef int (*timer_callback)(void* userdata, int len); //user callback
 struct timerData
 {
 	int 	timerFd;			//定时器的文件描述符
 	uint32 	interval;	//定时时间
-	void* 	cb;			//定时计时到，执行的回调函数
 	void* 	userdata;	//回调函数的数据指针
 	int   	len;		//数据长度
 };
 class timer
 {
 public:
-	timer(uint32 time, void* cb, void* userdate, int len);
+	timer(uint32 time,  void* userdate, int len);
 	int getTimerfd();
-	void* getCallBack();
-	void exec();
 	uint32 start();
 	void stop();
 	virtual ~timer();
+		//注册超时函数
+	template<class CLASS>
+	void AttachTimeoutFuc(CLASS* pObj, void (CLASS::* func)(void* , int ))
+	{
+		m_timeout.Connect(pObj, func);
+	}
+	//解注册
+	template<class CLASS>
+	void DetachTimeoutFuc(CLASS* pObj, void (CLASS::* func)(void* , int ))
+	{
+		m_timeout.Disconnect(pObj, func);
+	}
+	void emit()
+	{
+		m_timeout.Emit(m_interval.userdata ,m_interval.len);
+	}
 private:
 	int init();
 	timerData m_interval;
+		//信号源
+	Gallant::Signal2<void* , int >  m_timeout;
 };
 
 #endif /* SRC_TIMER_H_ */
